ac0f025be2b6bfc13712f30af819126d46d64f5b
WedyDQ10 <hosigumayuugi@gmail.com>
2014-09-25 21:22:00 +0900

    Merge Wi-Fi icon

--- src/com/wedy/systemuimod/NotificationiconPatcher.java
+++ src/com/wedy/systemuimod/NotificationiconPatcher.java
@@ -23,7 +23,6 @@ import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
 public class NotificationiconPatcher implements IXposedHookZygoteInit, IXposedHookInitPackageResources, IXposedHookLoadPackage {
 	private static XSharedPreferences preference = null;
 	private static String MODULE_PATH = null;
-	/*private static Context mGbContext;*/
 	
 	@Override
 	public void initZygote(StartupParam startupParam) throws Throwable {
@@ -33,33 +32,36 @@ public class NotificationiconPatcher implements IXposedHookZygoteInit, IXposedHo
 
 	@Override
 	public void handleInitPackageResources(InitPackageResourcesParam resparam) throws Throwable {
+		// For Change the number of tiles in a row
 		String s = preference.getString("list_key", "4");
 		int i = Integer.parseInt(s);
+		// For Change quicik settings width for Z3
 		String sz3 = preference.getString("key_widthz3", "90");
 		int iz3 = Integer.parseInt(sz3);
+		// Change battery charged alert level
 		String sbl = preference.getString("key_fullalert_et", "100");
 		int ibl = Integer.parseInt(sbl);
+		
 		if (!resparam.packageName.equals("com.android.systemui"))
 			return;
 
 		XModuleResources modRes = XModuleResources.createInstance(MODULE_PATH, resparam.res);
 
-		boolean isDataw = preference.getBoolean("key_dataw", false);
-		if(isDataw){
-			resparam.res.setReplacement("com.android.systemui", "drawable", "ic_notify_button_bg", modRes.fwd(R.drawable.ic_notify_button_bg));
-
-		}
+		// Brighten status bar icon
 		boolean isBrig = preference.getBoolean("key_brigi", false);
 		if(isBrig){
 			resparam.res.setReplacement("com.android.systemui", "dimen", "status_bar_icon_drawing_alpha", modRes.fwd(R.dimen.status_bar_icon_drawing_alpha));
 
 		}
+		
+		// Enlarge status bar icon
 		boolean isBigi = preference.getBoolean("key_bigi", false);
 		if(isBigi){
 			resparam.res.setReplacement("com.android.systemui", "dimen", "status_bar_icon_drawing_size", modRes.fwd(R.dimen.status_bar_icon_drawing_size));
 
 		}
 		
+		// Fix transparent status bar for 4.3
 		boolean isTranz1 = preference.getBoolean("key_tranz1", false);
 		if(isTranz1){
 			resparam.res.setReplacement("com.android.systemui", "color", "system_ui_opaque_background", 0x00000000);
@@ -67,6 +69,14 @@ public class NotificationiconPatcher implements IXposedHookZygoteInit, IXposedHo
 
 		}
 		
+		// Change notif. header button to transparent
+		boolean isDataw = preference.getBoolean("key_dataw", false);
+		if(isDataw){
+			resparam.res.setReplacement("com.android.systemui", "drawable", "ic_notify_button_bg", modRes.fwd(R.drawable.ic_notify_button_bg));
+
+		}
+		
+		// Hide notification and quick settings tab
 		boolean isNotab = preference.getBoolean("key_notab", false);
 
 		if(isNotab){
@@ -79,6 +89,8 @@ public class NotificationiconPatcher implements IXposedHookZygoteInit, IXposedHo
 			    }
 			    }); 
 		}
+		
+		// Move quick settings to notification tab and hide tabs
 		boolean isMovequick = preference.getBoolean("key_movequick", false);
 
 		if(isMovequick){
@@ -107,12 +119,16 @@ public class NotificationiconPatcher implements IXposedHookZygoteInit, IXposedHo
 		}
 		});
 		}
+		
+		// Change quicik settings height for Z2
 		boolean isHeighttool = preference.getBoolean("key_heighttool", false);
 		if(isHeighttool){
 			float f = Float.parseFloat(preference.getString("key_heightnum", "100.0"));
 resparam.res.setReplacement("com.android.systemui", "dimen", "notification_panel_tools_row_height", new DimensionReplacement(f,TypedValue.COMPLEX_UNIT_DIP));
 
 		}
+		
+		// Change quicik settings width for Z3
 		boolean isHeighttoolz3 = preference.getBoolean("key_heighttoolz3", false);
 		if(isHeighttoolz3){
 			resparam.res.setReplacement("com.android.systemui", "dimen", "notification_panel_tool_button_width",
@@ -120,18 +136,72 @@ resparam.res.setReplacement("com.android.systemui", "dimen", "notification_panel
 
 		}
 		
+		// Change the number of tiles in a row and unlimited items in quick settings
 		boolean isTranz144 = preference.getBoolean("key_tranz144", false);
 
 		if(isTranz144){
 			resparam.res.setReplacement("com.android.systemui", "integer", "config_maxToolItemsInARow", i);
 			resparam.res.setReplacement("com.android.systemui", "integer", "config_maxToolItemsInGrid", 99);
 		}
+		
+		// Change battery charged alert level
 		boolean iskey_fullalert = preference.getBoolean("key_fullalert", false);
 
 		if(iskey_fullalert){
 			resparam.res.setReplacement("com.android.systemui", "integer", "config_batteryChargedAlertLevel", ibl);
 		}
 		
+		// Merge Wi-Fi icon with mobile combo
+		boolean isWifim = preference.getBoolean("key_wifimerge", false);
+
+		if(isWifim){
+				resparam.res.hookLayout("com.android.systemui", "layout", "super_status_bar", new XC_LayoutInflated() {
+	            @Override
+	            public void handleLayoutInflated(LayoutInflatedParam liparam)
+	                    throws Throwable {
+		            FrameLayout wifi_combo = (FrameLayout) liparam.view.findViewById(liparam.res.getIdentifier("wifi_combo","id","com.android.systemui"));
+		            ViewGroup parent = (ViewGroup) wifi_combo.getParent();
+		            parent.removeView(wifi_combo);
+		            FrameLayout mobile_combo = (FrameLayout) liparam.view.findViewById(liparam.res.getIdentifier("mobile_combo","id","com.android.systemui"));
+		            ViewGroup mobile = (ViewGroup) mobile_combo.getParent();
+		            int index = parent.indexOfChild(mobile);
+		            parent.addView(wifi_combo, index + 1);
+	            }
+			});
+			resparam.res.setReplacement("com.android.systemui", "drawable", "stat_sys_wifi_signal_null", modRes.fwd(R.drawable.stat_sys_wifi_signal_null));
+			resparam.res.setReplacement("com.android.systemui", "drawable", "stat_sys_wifi_signal_0", modRes.fwd(R.drawable.stat_sys_wifi_signal_0));
+			resparam.res.setReplacement("com.android.systemui", "drawable", "stat_sys_wifi_signal_1", modRes.fwd(R.drawable.stat_sys_wifi_signal_1));
+			resparam.res.setReplacement("com.android.systemui", "drawable", "stat_sys_wifi_signal_2", modRes.fwd(R.drawable.stat_sys_wifi_signal_2));
+			resparam.res.setReplacement("com.android.systemui", "drawable", "stat_sys_wifi_signal_3", modRes.fwd(R.drawable.stat_sys_wifi_signal_3));
+			resparam.res.setReplacement("com.android.systemui", "drawable", "stat_sys_wifi_signal_4", modRes.fwd(R.drawable.stat_sys_wifi_signal_4));
+			resparam.res.setReplacement("com.android.systemui", "drawable", "stat_sys_wifi_signal_1_fully", modRes.fwd(R.drawable.stat_sys_wifi_signal_1_fully));
+			resparam.res.setReplacement("com.android.systemui", "drawable", "stat_sys_wifi_signal_2_fully", modRes.fwd(R.drawable.stat_sys_wifi_signal_2_fully));
+			resparam.res.setReplacement("com.android.systemui", "drawable", "stat_sys_wifi_signal_3_fully", modRes.fwd(R.drawable.stat_sys_wifi_signal_3_fully));
+			resparam.res.setReplacement("com.android.systemui", "drawable", "stat_sys_wifi_signal_4_fully", modRes.fwd(R.drawable.stat_sys_wifi_signal_4_fully));
+			resparam.res.setReplacement("com.android.systemui", "drawable", "stat_sys_wifi_limited_signal_0", modRes.fwd(R.drawable.stat_sys_wifi_limited_signal_0));
+			resparam.res.setReplacement("com.android.systemui", "drawable", "stat_sys_wifi_limited_signal_1", modRes.fwd(R.drawable.stat_sys_wifi_limited_signal_1));
+			resparam.res.setReplacement("com.android.systemui", "drawable", "stat_sys_wifi_limited_signal_2", modRes.fwd(R.drawable.stat_sys_wifi_limited_signal_2));
+			resparam.res.setReplacement("com.android.systemui", "drawable", "stat_sys_wifi_limited_signal_3", modRes.fwd(R.drawable.stat_sys_wifi_limited_signal_3));
+			resparam.res.setReplacement("com.android.systemui", "drawable", "stat_sys_wifi_limited_signal_4", modRes.fwd(R.drawable.stat_sys_wifi_limited_signal_4));
+			resparam.res.setReplacement("com.android.systemui", "drawable", "stat_sys_wifi_in", modRes.fwd(R.drawable.stat_sys_wifi_in));
+			resparam.res.setReplacement("com.android.systemui", "drawable", "stat_sys_wifi_out", modRes.fwd(R.drawable.stat_sys_wifi_out));
+			resparam.res.setReplacement("com.android.systemui", "drawable", "stat_sys_wifi_inout", modRes.fwd(R.drawable.stat_sys_wifi_inout));
+			resparam.res.setReplacement("com.android.systemui", "drawable", "somc_sys_wifi_in", modRes.fwd(R.drawable.stat_sys_wifi_in));
+			resparam.res.setReplacement("com.android.systemui", "drawable", "somc_sys_wifi_out", modRes.fwd(R.drawable.stat_sys_wifi_out));
+			resparam.res.setReplacement("com.android.systemui", "drawable", "somc_sys_wifi_inout", modRes.fwd(R.drawable.stat_sys_wifi_inout));
+			resparam.res.setReplacement("com.android.systemui", "drawable", "stat_sys_signal_0", modRes.fwd(R.drawable.stat_sys_signal_0));
+			resparam.res.setReplacement("com.android.systemui", "drawable", "stat_sys_signal_0_fully", modRes.fwd(R.drawable.stat_sys_signal_0_fully));
+			resparam.res.setReplacement("com.android.systemui", "drawable", "stat_sys_signal_1", modRes.fwd(R.drawable.stat_sys_signal_1));
+			resparam.res.setReplacement("com.android.systemui", "drawable", "stat_sys_signal_1_fully", modRes.fwd(R.drawable.stat_sys_signal_1_fully));
+			resparam.res.setReplacement("com.android.systemui", "drawable", "stat_sys_signal_2", modRes.fwd(R.drawable.stat_sys_signal_2));
+			resparam.res.setReplacement("com.android.systemui", "drawable", "stat_sys_signal_2_fully", modRes.fwd(R.drawable.stat_sys_signal_2_fully));
+			resparam.res.setReplacement("com.android.systemui", "drawable", "stat_sys_signal_3", modRes.fwd(R.drawable.stat_sys_signal_3));
+			resparam.res.setReplacement("com.android.systemui", "drawable", "stat_sys_signal_3_fully", modRes.fwd(R.drawable.stat_sys_signal_3_fully));
+			resparam.res.setReplacement("com.android.systemui", "drawable", "stat_sys_signal_4", modRes.fwd(R.drawable.stat_sys_signal_4));
+			resparam.res.setReplacement("com.android.systemui", "drawable", "stat_sys_signal_4_fully", modRes.fwd(R.drawable.stat_sys_signal_4_fully));
+		}
+		
+		// Fix Japanese translations
 		boolean isJpntr = preference.getBoolean("key_transjpn", false);
 
 		if(isJpntr){
@@ -149,6 +219,8 @@ resparam.res.setReplacement("com.android.systemui", "dimen", "notification_panel
 
 		if (!lpparam.packageName.equals("com.android.systemui"))
 			return;
+	
+	// Hide No SIM icon
 	boolean isNosim = preference.getBoolean("key_nosim", false);
 	if(isNosim){
 		try {
